8395b19ab1c2ab00607725b2f52ab59c4179ec28
Elias R <elias.rhoden@hotmail.com>
2018-05-27 22:28:03 +0200

    Ready to start working on GenomeCrossover!

---
+++ Tests/GenomeCrossoverTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GenomeCrossoverTest {
+
+
+    /**
+     * Tests the example from the AI-article Figure 4.
+     * */
+
+    @Test
+    void exampleFromText(){
+        //TODO: HÄR SLUTADE JAG
+    }
+
+
+}
---
+++ src/GenomeCrossover.java
@@ -0,0 +1,17 @@
+import Network.Genome;
+
+/**
+ * Created by eliasr on 2017-05-15.
+ */
+public class GenomeCrossover {
+
+    /**
+     * How two Networks/Genomes produces an offspring
+     * */
+    public Genome crossOver(Genome mom, Genome dad){
+        //TODO Hanky panky (IN MATH) here
+        return null;
+    }
+
+
+}
